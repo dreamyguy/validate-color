ca00b0f1a45ad75700e2a522081dbf6314163c69
Linus Unnebäck <linus@folkdatorn.se>
2020-01-17 21:31:56 +0000

    🎉 Add TypeScript typings

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export function validateHTMLColorName(input: string): boolean
+export function validateHTMLColor(input: string): boolean
+export default function validateColor(input: string): boolean
