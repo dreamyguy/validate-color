89c9662364ad94a0a751c639e6eaa438456d8fd3
Wallace Sidhrée <i@dreamyguy.com>
2020-05-22 23:07:46 +0200

    :rainbow: Update type definitions, ignore files, bump version

--- src/validate-color/index.d.ts
+++ src/validate-color/index.d.ts
@@ -1,4 +1,7 @@
-export function validateHTMLColorName(input: string): boolean
-export function validateHTMLColor(input: string): boolean
-export function validateHTMLColorHex(input: string): boolean
-export default function validateColor(input: string): boolean
+export function validateHTMLColor(input: string): boolean;
+export function validateHTMLColorHex(input: string): boolean;
+export function validateHTMLColorHsl(input: string): boolean;
+export function validateHTMLColorName(input: string): boolean;
+export function validateHTMLColorRgb(input: string): boolean;
+export function validateHTMLColorSpecialName(input: string): boolean;
+export default function validateColor(input: string): boolean;
